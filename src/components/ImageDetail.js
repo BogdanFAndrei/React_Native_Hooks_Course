0d67b2ea3b99a4ff0c475542557203cf2e420fa5
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-11 20:37:29 +0000

    style: Enhanced ImageDetail layout and styling

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = props => {
+const ImageDetail = ({imageSource, title, score}) => {
   return (
-    <View>
-      <Image source={props.imageSource} />
-      <Text>{props.title}</Text>
-      <Text>Image Score - {props.score}</Text>
+    <View 
+      style={styles.container}>
+        <Image source={imageSource} style={styles.image} />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.score}>Image Score- {score}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 15,
+  },
+  image: {
+    width: 250,
+    height: 150,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  score: {
+    color: '#666',
+    marginTop: 3,
+  },
+});
 
 export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score={9} />
       <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score={7} />
       <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score={4} />
@@ -12,6 +12,12 @@ const ImageScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+});
 
 export default ImageScreen;
