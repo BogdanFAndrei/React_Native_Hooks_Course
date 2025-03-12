4294525004477e883fc7c7023339b53298b70bed
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-12 23:15:44 +0000

    docs: add documentation to HomeScreen component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
+/**
+ * HomeScreen serves as the main navigation hub of the application.
+ * It provides buttons to navigate to all available demo screens.
+ * 
+ * @component
+ * @param {Object} props - Component props
+ * @param {Object} props.navigation - Navigation object provided by React Navigation
+ * @returns {React.ReactElement} A screen with navigation buttons to all demos
+ */
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
