6cef059e167a8d03a3230cd083e8d7dfc3db962d
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-13 20:47:54 +0000

    7.71 Leaning AlignItems with Flex

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,19 +3,22 @@ import {Text, View, StyleSheet} from 'react-native';
 
 const BoxScreen = () => {
     return <View style = {styles.viewStyle}>
-        <Text style={styles.textStyle}> Box Screen</Text>
+        <Text style={styles.textStyle}> Child #1</Text>
+        <Text style={styles.textStyle}> Child #2</Text>
+        <Text style={styles.textStyle}> Child #3</Text>
     </View>
 };
 
 const styles = StyleSheet.create({
     viewStyle: {
+        alignItems: 'flex-start',
         borderWidth: 3,
         borderColor: 'black'
     },
     textStyle: {
-        borderWidth: 10,
+        borderWidth: 3,
         borderColor: 'red',
-        margin: 20,
+    
 
     },
 });
