0326d247f0403defe2dbf41965485bd2d19ee337
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-13 20:03:43 +0000

    6.66 Creating a textinput and passig a state through

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,19 @@
-import react from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
+
     return <View>
+        <Text>Enter Name:0</Text>
         <TextInput 
             style={styles.input} 
             autoCapitalize='none'
             autoCorrect ={false}
+            value={name}
+            onChangeText={newValue => setName(newValue)}
         />
+        <Text>My name is {name}</Text>
     </View>
     
 };
